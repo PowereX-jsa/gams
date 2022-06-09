9d5e901a5d8f46244fec9e1b7142cbf621cfdd65
Patrik Beka <patrik.beka@gmail.com>
2022-06-09 13:12:14 +0200

    #300 Add getting bucket and filename that triggered lambda, add test s3 put event

--- gams-lambda-example/src/main/java/org/gams/integration/handlers/RequestHandler.java
+++ gams-lambda-example/src/main/java/org/gams/integration/handlers/RequestHandler.java
@@ -2,6 +2,7 @@ package org.gams.integration.handlers;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.events.S3Event;
+import com.amazonaws.services.lambda.runtime.events.models.s3.S3EventNotification.S3BucketEntity;
 import jakarta.inject.Singleton;
 import lombok.extern.slf4j.Slf4j;
 
@@ -10,9 +11,23 @@ import lombok.extern.slf4j.Slf4j;
 public class RequestHandler {
 
   public static String handleRequest(S3Event event, Context context) {
-    log.info("request received - event: '{}'", event);
+    log.info("request received - event: '{}'", event.toString());
 
-    return "ok";
+    return event.getRecords().stream()
+        .map(s3EventNotificationRecord -> {
+          log.info("notif record: '{}'", s3EventNotificationRecord.toString());
+
+          S3BucketEntity bucket = s3EventNotificationRecord.getS3()
+              .getBucket();
+          String file = s3EventNotificationRecord.getS3().getObject().getKey();
+
+          log.info("bucket: '{}'", bucket.toString());
+          log.info("file: '{}'", file);
+
+          return bucket.getName() + ": " + file;
+        })
+        .findFirst()
+        .orElse("no bucket found");
   }
 
 }
