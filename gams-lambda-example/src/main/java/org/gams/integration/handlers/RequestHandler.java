163fd4d388d514cb5ccc5cc90cd90a484ac5d440
Patrik Beka <patrik.beka@gmail.com>
2022-06-14 08:58:47 +0200

    #300 Rework handler using micronaut functions

--- gams-lambda-example/src/main/java/org/gams/integration/handlers/RequestHandler.java
+++ gams-lambda-example/src/main/java/org/gams/integration/handlers/RequestHandler.java
@@ -1,33 +1,52 @@
 package org.gams.integration.handlers;
 
-import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.events.S3Event;
 import com.amazonaws.services.lambda.runtime.events.models.s3.S3EventNotification.S3BucketEntity;
-import jakarta.inject.Singleton;
+import io.micronaut.core.annotation.Introspected;
+import io.micronaut.function.aws.MicronautRequestHandler;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.gams.integration.services.ManifestReader;
+import org.gams.integration.services.S3Service;
 
 @Slf4j
-@Singleton
-public class RequestHandler {
+@Introspected
+@RequiredArgsConstructor
+public class RequestHandler extends
+    MicronautRequestHandler<S3Event, String> {
 
-  public static String handleRequest(S3Event event, Context context) {
-    log.info("request received - event: '{}'", event.toString());
+  private final S3Service s3Service;
+  private final ManifestReader manifestReader;
 
-    return event.getRecords().stream()
+  @Override
+  public String execute(S3Event input) {
+    log.info("request received - event: '{}'", input.toString());
+
+    return Optional.ofNullable(input.getRecords().get(0))
         .map(s3EventNotificationRecord -> {
           log.info("notif record: '{}'", s3EventNotificationRecord.toString());
 
           S3BucketEntity bucket = s3EventNotificationRecord.getS3()
               .getBucket();
-          String file = s3EventNotificationRecord.getS3().getObject().getKey();
+          String manifestFileKey = s3EventNotificationRecord.getS3().getObject().getKey();
 
           log.info("bucket: '{}'", bucket.toString());
-          log.info("file: '{}'", file);
+          log.info("manifest: '{}'", manifestFileKey);
 
-          return bucket.getName() + ": " + file;
+          List<String> filenames = manifestReader.getFilenamesFromManifest(
+              manifestFileKey);
+
+          filenames.forEach(filenameS3Key -> {
+
+            Path localFilePath = Path.of("/tmp", Path.of(filenameS3Key).getFileName().toString());
+            s3Service.downloadFile(filenameS3Key, localFilePath.toFile());
+          });
+
+          return bucket.getName() + ": " + manifestFileKey;
         })
-        .findFirst()
         .orElse("no bucket found");
   }
-
 }
