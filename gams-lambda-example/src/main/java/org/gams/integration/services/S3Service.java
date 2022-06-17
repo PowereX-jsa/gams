f16edbff32eb23975a15d5472d94dfbf7790cf2c
Patrik Beka <patrik.beka@gmail.com>
2022-06-17 08:49:26 +0200

    #300 Add upload to s3 bucket

--- gams-lambda-example/src/main/java/org/gams/integration/services/S3Service.java
+++ gams-lambda-example/src/main/java/org/gams/integration/services/S3Service.java
@@ -5,12 +5,14 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectResult;
 import com.amazonaws.services.s3.model.S3Object;
 import jakarta.inject.Singleton;
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Path;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
@@ -25,9 +27,17 @@ public class S3Service {
   private static final String INPUT_DATA_BUCKET_NAME = "data-input-lambda-test";
   private static final String OUTPUT_DATA_BUCKET_NAME = "data-output-lambda-test";
 
-  // TODO
-  public void uploadFile() {
-    throw new UnsupportedOperationException("method not implemented");
+  public void uploadFile(String folder, File file) {
+    AmazonS3 client = createS3Client();
+
+    String s3Key = Path.of(folder, file.getName()).toString();
+
+    PutObjectResult result = client.putObject(
+        OUTPUT_DATA_BUCKET_NAME, s3Key, file);
+
+    log.info("successfully uploaded '{}' to s3 bucket: '{}' from local file: '{}'", s3Key,
+        OUTPUT_DATA_BUCKET_NAME, file);
+    log.debug("rawObjectMetadata='{}'", result.getMetadata().getRawMetadata());
   }
 
   public void downloadFile(String key, File targetFile) {
@@ -35,9 +45,13 @@ public class S3Service {
 
     ObjectMetadata objectMetadata = client.getObject(
         new GetObjectRequest(INPUT_DATA_BUCKET_NAME, key), targetFile);
-    log.info("successfuly download '{}' from s3 bucket: '{}' to local file: '{}'", key,
+    log.info("successfully download '{}' from s3 bucket: '{}' to local file: '{}'", key,
         INPUT_DATA_BUCKET_NAME, targetFile);
     log.debug("rawObjectMetadata='{}'", objectMetadata.getRawMetadata());
+  }
+
+  public String getParentOfS3File(String s3KeyFile) {
+    return Path.of(s3KeyFile).getParent().toString();
   }
 
   @SneakyThrows(IOException.class)
